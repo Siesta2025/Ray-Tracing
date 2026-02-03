b892dd156c438f1d50ac657eb21d513e8352ff2b
Siesta <wzl200611162022@icloud.com>
2026-02-03 15:38:19 +0800

    Chapter 3 accoplished

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
-#include "vec3.h"// For operator overloading
+#include "vec3.h"
+#include "ray.h"
+
+// Pay attention that vec3 can represent both colors and vectors
+vec3 color(const ray& r){ // Only read the ray, cannot modify it
+    vec3 unit_direction=unit_vector(r.direction()); // Normalize
+    float t=0.5*(unit_direction.y()+1.0); // Mapping y(-1~1) to (0~1) in order to control colors
+    return (1.0-t)*vec3(1.0,1.0,1.0)+t*vec3(0.5,0.7,1.0); // Blending white(0.0,0.0,0.0) and blue(0.5,0.7,1.0)
+}
 
 int main(){
     int nx=200;
     int ny=100;
     std::cout<<"P3\n"<<nx<<" "<<ny<<"\n255\n"; // The PPM format
+    vec3 lower_left_corner(-2.0,-1.0,-1.0);
+    vec3 horizontal(4.0,0.0,0.0);
+    vec3 vertical(0.0,2.0,0.0);
+    vec3 origin(0.0,0.0,0.0);
     for(int j=ny-1;j>=0;j--){
         for(int i=0;i<nx;i++){
-            //Cast to float, ensuring the result is a float between 0 and 1
-            vec3 col(float(i)/float(nx),float(j)/float(ny),0.2);
+            // The ratio
+            float u=float(i)/float(nx);
+            float v=float(j)/float(ny);
+            // Constructing the ray pointing to the destination
+            ray r(origin,lower_left_corner+u*horizontal+v*vertical);
+            // Creating corresponding color
+            vec3 col=color(r);
             // Scale to int between 0 and 255, 255.99 is a tricky multiplier, as int cast would simply truncate the float
             int ir=int(255.99*col[0]);
             int ig=int(255.99*col[1]);
